8e62eabb81ffa9cd6cf3ec19e3e9106df24026f1
Miki-Riako <miki_riako@qq.com>
2024-03-31 09:16:40 +0800

    test

--- src/test.c
+++ src/test.c
@@ -1,14 +1,35 @@
 #include <gtk/gtk.h>
 
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
+// 定义activate函数
+static void activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
 
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // 创建一个新窗口
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
+    // 显示窗口
     gtk_widget_show(window);
-    gtk_main();
-
-    return 0;
 }
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    // 创建一个新的GtkApplication
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+    // 运行应用程序，传入argc和argv
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    
+    // 清理
+    g_object_unref(app);
+
+    return status;
+}
+
+
+
 
