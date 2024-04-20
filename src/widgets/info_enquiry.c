ae0bb4d82314132ccd4b199ed8980e895947b296
Miki-Riako <miki_riako@qq.com>
2024-04-20 19:13:25 +0800

    done?

--- src/widgets/info_enquiry.c
+++ src/widgets/info_enquiry.c
@@ -2,6 +2,7 @@
 #include "../header.h"
 
 void infoEnquiryWidget();
+int selectSearchAttribute(int which);
 void simpleQuery(head_node *head);
 void combinedQuery(head_node *head);
 void fuzzyQuery(head_node *head);
@@ -56,51 +57,92 @@ void infoEnquiryWidget() {
     }
 }
 
+int selectSearchAttribute(int which) {
+    char index = '6';
+    switch (which) {
+    case '0': // 客户
+        printf("1. 名称\n2. 地区\n3. 地址\n4. 法人\n5. 规模\n6. 联系等级\n7. 邮箱\n8. 电话\n");
+        index = 8;
+        break;
+    case '1': // 联络人
+    case '2': // 业务员
+        printf("1. 名称\n2. 性别\n3. 生日\n4. 邮箱\n5. 电话\n6. 代表公司\n");
+        break;
+    }
+    char get[MAX_LENGTH];
+    infoInput(get, sizeof(get), "请选择属性以进行搜索：\n");
+    if (!isOneChar(get) || get[0] < '1' || get[0] > index) {
+        printf("无效的选择，请重新输入。\n");
+        return -1;
+    } else {
+        return get[0] - '0' - 1;
+    }
+}
+
 void simpleQuery(head_node *head) {
     int which = beforeInfo(head, "查询");
     if (which == -1) return;  // 无效的查询类型或无数据类型被选中
 
-    char queryName[MAX_LENGTH];
+    char queryValue[MAX_LENGTH];
     bool found = false;
+    int attributeIndex = selectSearchAttribute(which);
+
+    if (attributeIndex == -1) {
+        printf("无效的属性选择。\n");
+        return;
+    }
+
+    // 获取用户想要搜索的值
+    infoInput(queryValue, sizeof(queryValue), "请输入搜索值：");
+
+    printf("搜索中，请您耐心等待。\n");
+    head_node *newHead = (head_node *)malloc(sizeof(head_node));  // 分配内存
+    if (!newHead) {
+        fprintf(stderr, "内存分配失败！\n");
+        return;
+    }
+    loadFile(head);  // 加载数据
+    if (newHead->is_empty) printf("没有加载到任何数据。\n");
 
     // 根据用户选择决定查询内容和显示格式
     switch (which) {
     case 0: { // 查询客户
-        infoInput(queryName, sizeof(queryName), "请输入要查询的客户名称：");
-        node_cus *cusNode = head->next_cus;
-        printf("客户名 - 地区 - 地址 - 法人 - 规模 - 联系等级 - 邮箱 - 电话\n");
-        while (cusNode != NULL) {
-            if (isSameString(cusNode->customer.name, queryName)) {
-                printNode_cus(cusNode);
-                found = true;
-            }
-            cusNode = cusNode->next;
-        }
-        break;
+        searchCustomers(newHead->next_cus, queryValue, attributeIndex);
+        // infoInput(queryName, sizeof(queryName), "请输入要查询的客户名称：");
+        // node_cus *cusNode = head->next_cus;
+        // printf("客户名 - 地区 - 地址 - 法人 - 规模 - 联系等级 - 邮箱 - 电话\n");
+        // while (cusNode != NULL) {
+        //     if (isSameString(cusNode->customer.name, queryName)) {
+        //         printNode_cus(cusNode);
+        //         found = true;
+        //     }
+        //     cusNode = cusNode->next;
+        // }
+        // break;
     } case 1: { // 查询联络人
-        infoInput(queryName, sizeof(queryName), "请输入要查询的联络人名称：");
-        node_ctp *ctpNode = head->next_ctp;
-        printf("联络人名称 - 性别 - 生日 - 邮箱 - 电话 - 代表公司\n");
-        while (ctpNode != NULL) {
-            if (isSameString(ctpNode->contactPerson.name, queryName)) {
-                printNode_ctp(ctpNode);
-                found = true;
-            }
-            ctpNode = ctpNode->next;
-        }
-        break;
+        // infoInput(queryName, sizeof(queryName), "请输入要查询的联络人名称：");
+        // node_ctp *ctpNode = head->next_ctp;
+        // printf("联络人名称 - 性别 - 生日 - 邮箱 - 电话 - 代表公司\n");
+        // while (ctpNode != NULL) {
+        //     if (isSameString(ctpNode->contactPerson.name, queryName)) {
+        //         printNode_ctp(ctpNode);
+        //         found = true;
+        //     }
+        //     ctpNode = ctpNode->next;
+        // }
+        // break;
     } case 2: { // 查询业务员
-        infoInput(queryName, sizeof(queryName), "请输入要查询的业务员名称：");
-        node_emp *empNode = head->next_emp;
-        printf("业务员名称 - 性别 - 生日 - 邮箱 - 电话 - 代表公司\n");
-        while (empNode != NULL) {
-            if (isSameString(empNode->employee.name, queryName)) {
-                printNode_emp(empNode);
-                found = true;
-            }
-            empNode = empNode->next;
-        }
-        break;
+        // infoInput(queryName, sizeof(queryName), "请输入要查询的业务员名称：");
+        // node_emp *empNode = head->next_emp;
+        // printf("业务员名称 - 性别 - 生日 - 邮箱 - 电话 - 代表公司\n");
+        // while (empNode != NULL) {
+        //     if (isSameString(empNode->employee.name, queryName)) {
+        //         printNode_emp(empNode);
+        //         found = true;
+        //     }
+        //     empNode = empNode->next;
+        // }
+        // break;
     }}
     if (!found) {
         printf("没有找到匹配的信息。\n");
